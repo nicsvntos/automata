5c69631a1159d4cd38b02733af500a18e71a65ce
Nic <santos.nicole1010@gmail.com>
2026-02-28 22:38:29 +0800

    add text search

--- src/automata_search/dfa.py
+++ src/automata_search/dfa.py
@@ -52,3 +52,64 @@ def kmp_search(text, pattern):
             else:
                 i += 1
     return matches
+
+def kmp_search_with_path(text, pattern):
+    M = len(pattern)
+    N = len(text)
+
+    if M == 0: return 0, []
+
+    lps = compute_lps_array(pattern)
+
+    i = 0
+    j = 0
+
+    matches = 0
+    path_history = []
+
+    while i < N:
+        if pattern[j] == text[i]:
+            path_history.append({
+                'from': j,
+                'to': j + 1,
+                'char': text[i],
+                'type':'match'
+            })
+            i += 1
+            j += 1
+
+        if j == M:
+            matches += 1
+
+            prev_j = j
+            j = lps[j-1]
+            path_history.append({
+                'from': prev_j,
+                'to': j,
+                'char': 'MATCH',
+                'type': 'reset'
+            })
+        
+        elif i < N and pattern [j] != text[i]:
+            #mismatch
+            if j!=0:
+                #backtrack using LPS with dashed arrow
+                prev_j = j
+                j = lps[j-1]
+                path_history.append({
+                    'from': prev_j,
+                    'to': j,
+                    'char': text[i],
+                    'type': 'mismatch'
+                })
+            else:
+                #j is 0, and you can't backtrack
+                path_history.append({
+                    'from': 0,
+                    'to': 0,
+                    'char': text[i],
+                    'type': 'mismatch'
+                })
+
+                i += 1
+    return matches, path_history
